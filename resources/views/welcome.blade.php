c12810e519d5d39ef8a970a34a2ca47254a29398
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2022-08-30 16:01:02 -0400

    iniciando bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,18 +15,25 @@
             }
         </style>
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
 
 </head>
 <body>
     
-    <h1>Ok rodriguinho</h1>
+    @include('navbar')
     
+    <div class="container p-3">
     <div id="app">
-        <button @click="count++">
+        <button class="btn btn-primary" @click="count++">
             Count is: @{{ count }}
         </button>
     </div>
+    </div>
     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </body>
 </html>
